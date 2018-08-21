f19f14a6068a4a902706d81b2cf69ac79f50fbc8
alesoc91 <ale.socco@gmail.com>
2018-08-21 23:56:37 +0200

    aggiunta model CollaboratorRole

---
+++ app/CollaboratorRole.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CollaboratorRole extends Model
+{
+    public function collaborator()
+    {
+        return $this->hasMany('App\Collaborator');
+    }
+}
